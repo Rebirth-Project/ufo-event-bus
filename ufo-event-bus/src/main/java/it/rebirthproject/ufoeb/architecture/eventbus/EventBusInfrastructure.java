d1062c0ac3736cfff983a777a3049da8243918ea
patton <andrea.paternesi@gmail.com>
2023-01-08 11:03:29 +0100

    Removed complex shutdown code. It's useless.

--- ufo-event-bus/src/main/java/it/rebirthproject/ufoeb/architecture/eventbus/EventBusInfrastructure.java
+++ ufo-event-bus/src/main/java/it/rebirthproject/ufoeb/architecture/eventbus/EventBusInfrastructure.java
@@ -124,36 +124,12 @@ final class EventBusInfrastructure {
         logger.debug("Shutting down command for the bus system");
         try {
             sendShutdownStateManagerMessage();
-            //countDownLatch.await();
             busMemoryStateManagerThread.join();
-            shutdownAndAwaitTermination(workersPoolExecutor);
-//            workersPoolExecutor.shutdownNow();
-            
+            workersPoolExecutor.shutdownNow();            
         } catch (InterruptedException ex) {
             logger.error("Error during the shutdown", ex);
         }
 
-    }
-
-    void shutdownAndAwaitTermination(ExecutorService pool) {
-        // Disable new tasks from being submitted
-        pool.shutdown();
-        try {
-            // Wait a while for existing tasks to terminate
-            if (!pool.awaitTermination(60, TimeUnit.SECONDS)) {
-                // Cancel currently executing tasks forcefully
-                pool.shutdownNow();
-                // Wait a while for tasks to respond to being cancelled
-                if (!pool.awaitTermination(60, TimeUnit.SECONDS)) {
-                    System.err.println("Pool did not terminate");
-                }
-            }
-        } catch (InterruptedException ex) {
-            // (Re-)Cancel if current thread also interrupted
-            pool.shutdownNow();
-            // Preserve interrupt status
-            Thread.currentThread().interrupt();
-        }
     }
 
     /**
